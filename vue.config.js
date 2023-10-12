b46fdf3fbd4ee9eefa692c1cbdc6fc54ececbb45
Hooorus <2479711422@qq.com>
2023-10-12 12:35:16 +0800

    finish the structure

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ],
-  //新增
-  publicPath: 'mshive-online',//github仓库名
-  assetsDir: 'mshive-online',//同上，否则访问不到静态资源
-  outputDir:"dist",
+    transpileDependencies: [
+        'vuetify'
+    ],
+    //新增
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/mshive-online/'
+        : '/'
 })
